e0058c7128c7c34b877e68acfb15e113809b6afe
teresacabell <teresacabll@gmail.com>
2022-02-10 20:33:22 -0600

    added database models tag and producttag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { truncate } = require('../config/connection');
 
 const sequelize = require('../config/connection');
 
@@ -6,7 +7,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
